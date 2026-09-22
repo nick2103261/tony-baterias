870717faf2b27b0c9beb6c2bae2dc08c22c13c8b
Nicolas <nicolashernandes2103@gmail.com>
2026-09-22 20:29:15 -0300

    Até aqui ta puxando o que foi criado la no Usuario.php

--- app/controllers/UsuarioController.php
+++ app/controllers/UsuarioController.php
@@ -2,5 +2,22 @@
 
 class UsuarioController
 {
-    
+    private Usuario $usuarioModel;
+
+    public function __construct()
+    {
+        $this->usuarioModel = new Usuario();
+
+    }
+
+    public function listar(): array
+    {
+        return $this->usuarioModel->listarTodos();
+    }
+
+    public function buscar(int $id): ?array
+    {
+        return $this->usuarioModel->buscarPorId($id);
+    }
+
 }
